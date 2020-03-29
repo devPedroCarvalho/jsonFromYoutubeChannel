d4ed07523dba9caf8e3c53c5bfbfe73e3eec41c3
Pedro Carvalho <pedro.carvalho@hhsurge.com>
2020-03-29 17:23:32 +0100

    RecycleView finish

--- app/src/main/java/com/keezag/jsonyoutube/MainAdapter.kt
+++ app/src/main/java/com/keezag/jsonyoutube/MainAdapter.kt
@@ -1,24 +1,34 @@
 package com.keezag.jsonyoutube
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.layout_list.view.*
 
 class MainAdapter : RecyclerView.Adapter<CustomViewHolder>() {
+
+    val videoTitles = listOf("Primeiro titulo","Segundo titulo", "Terceiro titulo","quarto")
+
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+return videoTitles.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-        TODO("Not yet implemented")
+        // create a new view
+        val textView = LayoutInflater.from(parent.context).inflate(R.layout.layout_list, parent, false)
+        return CustomViewHolder(textView)
+
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val videoTitles = videoTitles[position]
+        holder.view.txt_titleVideo.text = videoTitles
+
     }
 
 }
 
-class CustomViewHolder(v: View): RecyclerView.ViewHolder(v){
+class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view){
 
 }
--- app/src/main/java/com/keezag/jsonyoutube/view/MainActivity.kt
+++ app/src/main/java/com/keezag/jsonyoutube/view/MainActivity.kt
@@ -3,6 +3,7 @@ package com.keezag.jsonyoutube.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.keezag.jsonyoutube.MainAdapter
 import com.keezag.jsonyoutube.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         //recycleView
         recyclerView_main.layoutManager = LinearLayoutManager(this)
-       // recyclerView_main.adapter = ??
+        recyclerView_main.adapter = MainAdapter()
 
 
     }
